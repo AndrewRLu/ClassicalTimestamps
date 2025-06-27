e4563dc30f4b55d0e2366493e4a9792e79e5d226
AndrewRLu <88295464+AndrewRLu@users.noreply.github.com>
2025-06-26 19:11:33 -0700

    prevent CORS block

--- ntl_functions/getTopLevelComments.mjs
+++ ntl_functions/getTopLevelComments.mjs
@@ -1,15 +1,24 @@
-// import fetch from 'node-fetch'
-
 export const handler = async (event, context) => {
 
-    const maxResults = 100;
-    const youtubeApiKey = process.env.YOUTUBE_API_KEY;
-    const { videoID, nextPageToken } = JSON.parse(event.body);
-    const callResponse = await fetch(`https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet&maxResults=${maxResults}&order=relevance&videoId=${videoID}&key=${youtubeApiKey}&pageToken=${nextPageToken}`);
-    const callResponseJSON = await callResponse.json();
-    const pageComments = callResponseJSON.items.map(comment => comment.snippet.topLevelComment.snippet.textOriginal);
-    let npt = callResponseJSON.nextPageToken || "";
+  //prevent getting blocked
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': 'https://www.youtube.com'
+        }
+    };
+  }
 
+  
+  //youtube api req
+  const maxResults = 100;
+  const youtubeApiKey = process.env.YOUTUBE_API_KEY;
+  const { videoID, nextPageToken } = JSON.parse(event.body);
+  const callResponse = await fetch(`https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet&maxResults=${maxResults}&order=relevance&videoId=${videoID}&key=${youtubeApiKey}&pageToken=${nextPageToken}`);
+  const callResponseJSON = await callResponse.json();
+  const pageComments = callResponseJSON.items.map(comment => comment.snippet.topLevelComment.snippet.textOriginal);
+  let npt = callResponseJSON.nextPageToken || "";
 
   return {
     statusCode: 200,
@@ -18,11 +27,3 @@ export const handler = async (event, context) => {
     }),
   }
 }
-
-// async function getTopLevelComments(videoID, nextPageToken){
-//     console.log(`getting top level comments`);
-
-
-//     console.log(`comments with token "${nextPageToken}": ${pageComments}`);
-//     return {comments: pageComments, token: nextPageToken};
-// }
