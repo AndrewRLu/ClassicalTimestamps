e605b794ee86eb6f38557db74480f9422a603ea0
AndrewRLu <88295464+AndrewRLu@users.noreply.github.com>
2025-06-26 20:00:31 -0700

    CORS return update

--- ntl_functions/getTopLevelComments.mjs
+++ ntl_functions/getTopLevelComments.mjs
@@ -23,6 +23,10 @@ export const handler = async (event, context) => {
 
   return {
     statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': 'https://www.youtube.com', 
+      'Content-Type': 'application/json',
+    },
     body: JSON.stringify({
       comments: pageComments, token: npt
     }),
